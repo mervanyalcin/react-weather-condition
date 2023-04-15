609d2bd6b9edf8913d2e9c1b3915333b6585ede3
Mervan37 <mervannyalcinn@gmail.com>
2023-04-15 16:49:13 +0300

    For Theises

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import allcity from "./AllCity";
-
 const store = configureStore({
   reducer: {
     allcity,
   },
 });
-
 export default store;
